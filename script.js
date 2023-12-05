003228c2fbc93fe0d531800f9b96a45902be1643
Erica Wolf (she/her) <ebwolf@gmail.com>
2023-12-05 14:15:15 -0500

    Log location

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
- function myFunction() {
+function myFunction() {
     window.open("tel:+15678675309");
   }
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(logPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function logPosition(position) {
+   console.log("Latitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude);
+}
